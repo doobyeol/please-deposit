f88871f84accb2aeebbdb20aeca8c9028ae9fecb
iskwon <kis6905@gmail.com>
2022-12-31 17:19:43 +0900

    init front

---
+++ front/src/plugins/vuetify.ts
@@ -0,0 +1,22 @@
+import "vuetify/styles";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
+import { createVuetify } from "vuetify";
+
+export default createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "dark",
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 class="p-4">This is an home page</h1>
+    <v-icon icon="mdi-home" />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.p-4 {
+  padding: 4px;
+}
+</style>
